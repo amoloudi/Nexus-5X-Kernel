f4842d1c570fd3afc642a6607f6c63d301de987e
amoloudi <mohammadaminmoloudi12369@gmail.com>
2017-11-06 22:41:26 +0330

    time.c

--- time.c
+++ time.c
@@ -1,13 +1,15 @@
+#define _GNU_SOURCE			//Access to nonStandard GNU/Linux functions (Needed for syscall)
+#include <unistd.h> 			//Access to POSIX (Needed for syscall)
 #include <sys/syscall.h>
 #include <stdio.h>
-#include <time.h> // JUST FOR EPOCH TO DATE CONVERSION
+#include <time.h> 			//EPOCH TO Date Conversion
 
 int main()
 {
     long time;
-    time = syscall(201);
+    time = syscall(201);		//getting time in seconds
     
-    printf("%s", ctime(&time));
+    printf("%s", ctime(&time));		//convert time in seconds to "DayOfWeek month day hh:mm:ss year"
     
     return 0;
 }
